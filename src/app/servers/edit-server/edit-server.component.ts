597933d2109f0c0ba3186a2cb511b952464b9021
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2023-01-05 14:31:51 +0300

    first commit

---
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+  serverName = '';
+  serverStatus = '';
+  allowEdit = false;
+
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((queryParams: Params)=>{ 
+      this.allowEdit = queryParams["allowEdit"] === "1" ? true : false;
+    })
+    const id  = +this.route.snapshot.params["id"];
+    this.server = this.serversService.getServer(id);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+}
